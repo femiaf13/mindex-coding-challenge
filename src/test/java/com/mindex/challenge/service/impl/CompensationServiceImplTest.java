42c04b8f1918137edaec2aaeb8c6ca29b20bd47e
Frank Femia <femiaf13@gmail.com>
2021-11-24 16:09:23 -0500

    Add tests for compensation

---
+++ src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
@@ -0,0 +1,74 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.service.EmployeeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CompensationServiceImplTest {
+
+    private String compensationUrl;
+    private String compensationIdUrl;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    // This is for creating a compensation
+    private String testEmployee;
+
+    @Before
+    public void setup() {
+        compensationUrl = "http://localhost:" + port + "/compensation";
+        compensationIdUrl = "http://localhost:" + port + "/compensation/{id}";
+        testEmployee = "03aa1462-ffa9-4978-901b-7c001562cf6f";
+    }
+
+    @Test
+    public void testCreateReadUpdate() {
+        Compensation testCompensation = new Compensation();
+        testCompensation.setEmployeeId(testEmployee);
+        testCompensation.setSalary(10000000);
+        testCompensation.setEffectiveDate("1/1/2020");
+
+        // Create checks
+        Compensation createdCompensation = restTemplate.postForEntity(compensationUrl, testCompensation, Compensation.class).getBody();
+
+        assertNotNull(createdCompensation.getEmployeeId());
+        assertCompensationEquivalence(testCompensation, createdCompensation);
+
+
+        // Read checks
+        Compensation readCompensation = restTemplate.getForEntity(compensationIdUrl, Compensation.class, createdCompensation.getEmployeeId()).getBody();
+        assertEquals(createdCompensation.getEmployeeId(), readCompensation.getEmployeeId());
+        assertCompensationEquivalence(createdCompensation, readCompensation);
+    }
+
+    private static void assertCompensationEquivalence(Compensation expected, Compensation actual) {
+        assertEquals(expected.getEmployeeId(), actual.getEmployeeId());
+        // For doubles you need an acceptable delta I guess?
+        assertEquals(expected.getSalary(), actual.getSalary(), 0);
+        assertEquals(expected.getEffectiveDate(), actual.getEffectiveDate());
+    }
+}
