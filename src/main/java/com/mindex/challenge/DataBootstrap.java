7f5c652d75afb3e4d6ef44f2778dcdde8c2b6a93
Frank Femia <femiaf13@gmail.com>
2021-11-24 15:22:21 -0500

    Bootstrap some compensation data too

--- src/main/java/com/mindex/challenge/DataBootstrap.java
+++ src/main/java/com/mindex/challenge/DataBootstrap.java
@@ -1,7 +1,9 @@
 package com.mindex.challenge;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mindex.challenge.dao.CompensationRepository;
 import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,6 +15,10 @@ import java.io.InputStream;
 @Component
 public class DataBootstrap {
     private static final String DATASTORE_LOCATION = "/static/employee_database.json";
+    private static final String COMPENSATION_LOCATION = "/static/compensation_database.json";
+
+    @Autowired
+    private CompensationRepository compensationRepository;
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -22,9 +28,12 @@ public class DataBootstrap {
 
     @PostConstruct
     public void init() {
+        InputStream inputStreamCompensation = this.getClass().getResourceAsStream(COMPENSATION_LOCATION);
+
         InputStream inputStream = this.getClass().getResourceAsStream(DATASTORE_LOCATION);
 
         Employee[] employees = null;
+        Compensation[] compensations = null;
 
         try {
             employees = objectMapper.readValue(inputStream, Employee[].class);
@@ -34,6 +43,16 @@ public class DataBootstrap {
 
         for (Employee employee : employees) {
             employeeRepository.insert(employee);
+        }
+
+        try {
+            compensations = objectMapper.readValue(inputStreamCompensation, Compensation[].class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (Compensation compensation : compensations) {
+            compensationRepository.insert(compensation);
         }
     }
 }
