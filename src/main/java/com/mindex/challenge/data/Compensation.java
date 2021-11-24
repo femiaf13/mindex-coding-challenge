3a240b72c2a60f77f42f536d6e825511074dbb1b
Frank Femia <femiaf13@gmail.com>
2021-11-24 15:21:55 -0500

    Initial implementation of compensation

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,36 @@
+package com.mindex.challenge.data;
+
+import java.util.Date;
+
+public class Compensation {
+    private String employeeId;
+    private double salary;
+    private String effectiveDate;
+
+    public Compensation() {
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+}
