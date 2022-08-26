b3de8e0121adea986088adedd1091fdb4c123251
Frank Femia <femiaf13@gmail.com>
2022-08-26 15:35:52 -0400

    Added a negative test for compensation

--- src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
@@ -3,7 +3,6 @@ package com.mindex.challenge.service.impl;
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
-import com.mindex.challenge.service.EmployeeService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,13 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
@@ -30,31 +28,31 @@ public class CompensationServiceImplTest {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @Autowired
-    private EmployeeService employeeService;
-
     @LocalServerPort
     private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    // This is for creating a compensation
+    /**Employee without a compensation record */
     private Employee testEmployee;
+    /** Employee with a compensation record already created */ 
+    private Employee testEmployeeWithCompensation;
+
 
     @Before
     public void setup() {
         compensationUrl = "http://localhost:" + port + "/compensation";
         compensationIdUrl = "http://localhost:" + port + "/compensation/{id}";
         testEmployee =  employeeRepository.findByEmployeeId("62c1084e-6e34-4630-93fd-9153afb65309");
+        testEmployeeWithCompensation = employeeRepository.findByEmployeeId("03aa1462-ffa9-4978-901b-7c001562cf6f");
     }
 
     /**
      * There should be more tests here. Currently this only tests the 
-     * "happy path" which is far from everything that should be tested
+     * "happy path" and one fail case which is far from everything that should be tested
      * 
      * Examples: 
-     * POSTing a compensation for an employee twice
      * GETing a compensation that hasn't been created
      * GETing a compensation for an employee that doesn't exist
      * 
@@ -75,10 +73,22 @@ public class CompensationServiceImplTest {
         assertNotNull(createdCompensation.getEmployee());
         assertCompensationEquivalence(testCompensation, createdCompensation);
 
-
         // Read checks
         Compensation readCompensation = restTemplate.getForEntity(compensationIdUrl, Compensation.class, createdCompensation.getEmployee().getEmployeeId()).getBody();
         assertCompensationEquivalence(createdCompensation, readCompensation);
+    }
+
+    @Test
+    public void testCreateTwice() {
+        Compensation testCompensation = new Compensation();
+        testCompensation.setEmployee(testEmployeeWithCompensation);
+        testCompensation.setSalary(10000000);
+        testCompensation.setEffectiveDate("1/1/2020");
+
+        // Create should fail
+        ResponseEntity<Compensation> createdCompensation = restTemplate.postForEntity(compensationUrl, testCompensation, Compensation.class);
+        assertNotEquals(HttpStatus.OK, createdCompensation.getStatusCode());
+        
     }
 
     // Overriding equals for Compensation would remove the need for this
