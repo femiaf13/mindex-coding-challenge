0ae9e9be44854c19ee0eef81ea62d3d3df58a06c
Frank Femia <femiaf13@gmail.com>
2022-08-25 21:58:45 -0400

    Update tests for compensation taking Employee

--- src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.mindex.challenge.service.impl;
 
+import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.service.EmployeeService;
@@ -27,6 +28,9 @@ public class CompensationServiceImplTest {
     private String compensationIdUrl;
 
     @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
     private EmployeeService employeeService;
 
     @LocalServerPort
@@ -36,37 +40,60 @@ public class CompensationServiceImplTest {
     private TestRestTemplate restTemplate;
 
     // This is for creating a compensation
-    private String testEmployee;
+    private Employee testEmployee;
 
     @Before
     public void setup() {
         compensationUrl = "http://localhost:" + port + "/compensation";
         compensationIdUrl = "http://localhost:" + port + "/compensation/{id}";
-        testEmployee = "03aa1462-ffa9-4978-901b-7c001562cf6f";
+        testEmployee =  employeeRepository.findByEmployeeId("62c1084e-6e34-4630-93fd-9153afb65309");
     }
 
+    /**
+     * There should be more tests here. Currently this only tests the 
+     * "happy path" which is far from everything that should be tested
+     * 
+     * Examples: 
+     * POSTing a compensation for an employee twice
+     * GETing a compensation that hasn't been created
+     * GETing a compensation for an employee that doesn't exist
+     * 
+     * In an effort to not spend *too* much time on this I will leave this note 
+     * instead of implementing
+     */
+
     @Test
-    public void testCreateReadUpdate() {
+    public void testCreateRead() {
         Compensation testCompensation = new Compensation();
-        testCompensation.setEmployeeId(testEmployee);
+        testCompensation.setEmployee(testEmployee);
         testCompensation.setSalary(10000000);
         testCompensation.setEffectiveDate("1/1/2020");
 
         // Create checks
         Compensation createdCompensation = restTemplate.postForEntity(compensationUrl, testCompensation, Compensation.class).getBody();
 
-        assertNotNull(createdCompensation.getEmployeeId());
+        assertNotNull(createdCompensation.getEmployee());
         assertCompensationEquivalence(testCompensation, createdCompensation);
 
 
         // Read checks
-        Compensation readCompensation = restTemplate.getForEntity(compensationIdUrl, Compensation.class, createdCompensation.getEmployeeId()).getBody();
-        assertEquals(createdCompensation.getEmployeeId(), readCompensation.getEmployeeId());
+        Compensation readCompensation = restTemplate.getForEntity(compensationIdUrl, Compensation.class, createdCompensation.getEmployee().getEmployeeId()).getBody();
         assertCompensationEquivalence(createdCompensation, readCompensation);
     }
 
+    // Overriding equals for Compensation would remove the need for this
+    // but I'm choosing to maintain the pattern already present in the tests
     private static void assertCompensationEquivalence(Compensation expected, Compensation actual) {
-        assertEquals(expected.getEmployeeId(), actual.getEmployeeId());
+        Employee expectedEmployee = expected.getEmployee();
+        Employee actuaEmployee = actual.getEmployee();
+
+        // This violates DRY and given more time *should* instead be handled by both 
+        // Employee and Compensation overriding equals for direct comparisons
+        assertEquals(expectedEmployee.getFirstName(), actuaEmployee.getFirstName());
+        assertEquals(expectedEmployee.getLastName(), actuaEmployee.getLastName());
+        assertEquals(expectedEmployee.getDepartment(), actuaEmployee.getDepartment());
+        assertEquals(expectedEmployee.getPosition(), actuaEmployee.getPosition());
+
         // For doubles you need an acceptable delta I guess?
         assertEquals(expected.getSalary(), actual.getSalary(), 0);
         assertEquals(expected.getEffectiveDate(), actual.getEffectiveDate());
